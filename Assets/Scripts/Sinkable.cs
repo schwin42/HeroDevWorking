c9c95deac24e604315b19bf96f665038fb6d4d11
sstensager <30914799+sstensager@users.noreply.github.com>
2018-07-26 21:48:59 -0700

    Added sinkable cube

---
+++ Assets/Scripts/Sinkable.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sinkable : MonoBehaviour {
+
+    Rigidbody rb;
+
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        rb.isKinematic = false;
+    }
+}

f6eb2baffac06f3e39ea9ad577a5c2654dfd1a77
Paul Schwin <schwin42@gmail.com>
2018-09-01 14:32:00 -0700

    POC voxel object destruction

---
+++ Assets/Scripts/VrInputManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VrInputManager : MonoBehaviour
+{
+
+	public static SteamVR_TrackedController[] devices;
+
+	// Use this for initialization
+	void Awake()
+	{
+		devices = GetComponentsInChildren<SteamVR_TrackedController>();
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+
+	}
+}
